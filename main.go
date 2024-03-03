8218f56a6fbf8ee00c511000c6afe6fcdc86adc8
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-03-03 19:26:05 -0300

    almost done with rendering html

--- main.go
+++ main.go
@@ -7,14 +7,38 @@ import (
   "net/http"
 )
 
+type Film struct {
+  Title       string
+  Director    string
+}
+
 func main(){
   fmt.Println("Hello.")
   
   h1 := func(w http.ResponseWriter, r *http.Request){
     tmpl := template.Must(template.ParseFiles("index.html"))
+
+    films := map[string][]Film{
+     "Films": {
+				{Title: "The Godfather", Director: "Francis Ford Coppola"},
+				{Title: "Blade Runner", Director: "Ridley Scott"},
+				{Title: "The Thing", Director: "John Carpenter"},
+			},    
+    }
+    tmpl.Execute(w, films)
+  }  
+
+  h2 := func (w http.ResponseWriter, r *http.Request) {
+    title := r.PostFormValue("title") 
+    director := r.PostFormValue("director")
+    htmlStr := fmt.Sprintf("<div class='border border-gray-700 rounded-xl p-4 mt-4'><p>%s - %s</p></div>", title, director)
+    tmpl, _ := template.New("t").Parse(htmlStr)
     tmpl.Execute(w, nil)
+
   }
 
+
   http.HandleFunc("/", h1)
+  http.HandleFunc("/add-film/", h2)
   log.Fatal(http.ListenAndServe(":8000", nil))
 }
